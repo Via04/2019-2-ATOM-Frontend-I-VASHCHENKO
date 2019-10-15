1fa4aaf1fbbdbdf807179ab0e638a027130fcf98
Ivan Vashchenko <via0408@gmail.com>
2019-10-15 18:50:59 +0300

    initial commit

--- web-components/src/components/MessageForm.js
+++ web-components/src/components/MessageForm.js
@@ -2,11 +2,26 @@ const template = document.createElement('template');
 template.innerHTML = `
     <style>
         form-input {
-            width: 100%;
+            width: auto;
         }
 
+        .top-panel {
+            background: darkviolet;
+            height 130px;
+            width: 100%;
+            position: fixed;
+            top: 0;
+        }
+        
         .result {
-            color: red;
+            background: #dac6eb;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-end;
+            padding: 50px;
+            overflow: hidden;
+            margin-top: 100px;
+            margin-bottom: 30px;
         }
 
         input[type=submit] {
@@ -14,8 +29,11 @@ template.innerHTML = `
         }
     </style>
     <form>
+        <div class="top-panel">
+            <h1 style="text-align: center">Chat header</h1>
+        </div>
         <div class="result"></div>
-        <form-input name="message-text" placeholder="Введите сообщеине"></form-input>
+        <form-input name="message-text" placeholder="Введите сообщение"></form-input>
     </form>
 `;
 
@@ -30,11 +48,21 @@ class MessageForm extends HTMLElement {
 
         this.$form.addEventListener('submit', this._onSubmit.bind(this));
         this.$form.addEventListener('keypress', this._onKeyPress.bind(this));
+        const messages = localStorage.getItem('messages').split('/$#@');
+        for (let element = 0; element < messages.length; element++) {
+          const $content = document.createElement('message-box');
+          const arr = messages[element].split('@#$%/');
+          $content.authorV = arr[0];
+          $content.textV = arr[1];
+          $content.timeV = arr[2];
+          this.$message.appendChild($content);
+          this.$message.scrollTop = this.$message.scrollHeight;
+        }
     }
 
     _onSubmit (event) {
         event.preventDefault();
-        this.$message.innerText = this.$input.value;
+        this.$input.value = this.$input.value.replace(/\s+/g, ' ').replace(/^\s|\s$/g, '');
     }
 
     _onKeyPress (event) {
