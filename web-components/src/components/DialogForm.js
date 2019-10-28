92ea44a072ce61efeaab2a6590fa537007d2e067
Ващенко Иван <via0408@gmail.com>
2019-10-28 23:12:36 +0300

    DialogForm

--- web-components/src/components/DialogForm.js
+++ web-components/src/components/DialogForm.js
@@ -65,4 +65,76 @@ class MainScreen extends HTMLElement {
             this.setAttribute('name', name);
         };
     }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.arr = [];
+        JSON.parse(localStorage.getItem('users')).forEach((element) => {
+            if (element.slice(0, newValue.length) === newValue) {
+                this.arr.push(element);
+            }
+        });
+        this.$dialog.innerHTML = '';
+        this.arr.forEach((element) => {
+            const $chat = document.createElement('dialog-container');
+            $chat.name = element;
+            const arr = JSON.parse(localStorage.getItem(element));
+            if (arr.length > 0) {
+                [$chat.message, $chat.date] = arr.slice(-1)[0];
+                $chat.check = '&#10003';
+            } else {
+                $chat.date = '';
+                $chat.message = '';
+            }
+            $chat.addEventListener('click', this.enterDialog.bind(this, element));
+            this.$dialog.appendChild($chat);
+        });
+    }
+
+    onclick() {
+        if (this.inputStatus) {
+            const $input = this.shadowRoot.querySelector('dialog-input');
+            $input.remove();
+            this.inputStatus = false;
+        } else {
+            const $input = document.createElement('dialog-input');
+            this.$nameInput.appendChild($input);
+            this.inputStatus = true;
+        }
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        const $input = this.shadowRoot.querySelector('dialog-input');
+        if ($input.value !== '') {
+            const data = JSON.parse(localStorage.getItem('users'));
+            if (data.includes($input.value)) {
+                alert('Данный диалог уже существует');
+            } else {
+                data.push($input.value);
+                localStorage.setItem('users', JSON.stringify(data));
+                localStorage.setItem($input.value, JSON.stringify([]));
+                const $chat = document.createElement('dialog-container');
+                $chat.name = $input.value;
+                $chat.date = '';
+                $chat.message = '';
+                $chat.addEventListener('click', this.enterDialog.bind(this, $input.value));
+                this.$dialog.appendChild($chat);
+                $input.value = '';
+            }
+        } else {
+            alert('Введите название диалога');
+        }
+    }
+
+    onKeyPress(event) {
+        if (event.keyCode === 13) {
+            this.$name_input.dispatchEvent(new Event('submit'));
+        }
+    }
+
+    set     enter(value) {
+        this.enterDialog = value;
+    }
 }
+
+customElements.define('main-screen', MainScreen);
