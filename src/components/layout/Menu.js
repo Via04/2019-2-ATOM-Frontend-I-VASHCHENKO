f3a1362a68f0b744b99089d9bbec399a10bcd420
Ващенко Иван <via0408@gmail.com>
2019-12-04 17:10:12 +0300

    not all fixed

---
+++ src/components/layout/Menu.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import SignedIn from './SignedIn';
+import SignedOut from './SignedOut';
+import styles from '../../styles/menu.module.css'
+
+
+const Menu = (props) => {
+	const [login, setLogin] = useState('Some UID');
+	const loginStatus = login ? <SignedIn/> : <SignedOut/>;
+	const { visible } = props;
+	if(visible) {
+		return (
+			<div className={styles.menu}>
+				<nav>
+					{loginStatus}
+				</nav>
+			</div>
+		);
+	}
+
+	return null;
+}
+
+Menu.propTypes = {
+	visible: PropTypes.bool.isRequired,
+};
+export default Menu;
