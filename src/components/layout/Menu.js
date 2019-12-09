affe6fe1d761eb58d2d7c6c81f7a97a92ce504c9
Ващенко Иван <via0408@gmail.com>
2019-12-09 07:25:22 +0300

    build fix

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -6,6 +6,7 @@ import styles from '../../styles/menu.module.css';
 
 const Menu = (props) => {
 	const [login, setLogin] = useState('Some UID');
+	setLogin('some UID');
 	const loginStatus = login ? <SignedIn /> : <SignedOut />;
 	const { visible } = props;
 	if (visible) {
